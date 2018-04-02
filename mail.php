98078449acb8f4253a05519b619c4f3a97856dd8
Kesara Wimal <kesarawimal9@gmail.com>
2018-04-02 21:39:18 +0530

    final

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  $to = "kesarawimal9@gmail.com";
+  $headers = "From: " . $email;
+
+  mail($to,$name,$message,$headers);
+
+    header('Location: http://kesara.ml/');
+}
+?>
